b474bc3f8aaa6b71215f4ecb11f58e7780b0a30a
Jeff Hyder <JHyder@polartec.com>
2017-12-22 11:25:48 -0500

    2

---
+++ SpikeContainer/DataEntitiy/MesDbEntities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.EntityClient;
+using PFCS.Classes;
+
+namespace PFCS.DataEntities
+{
+    public partial class MesDbEntities : DbContext
+    {
+        public MesDbEntities(bool custom)
+            : base(
+                new EntityConnectionStringBuilder
+                {
+                    Metadata =
+                        "res://*/DataEntities.MesDbModel.csdl|res://*/DataEntities.MesDbModel.ssdl|res://*/DataEntities.MesDbModel.msl",
+                    Provider = "System.Data.SqlClient",
+                    ProviderConnectionString = Data.Global.LoggedIn && !Data.Global.Admin
+                        ? $"data source = MANUFACTURING; initial catalog = {Data.Global.Catalog}; " +
+                          $"integrated security = True; MultipleActiveResultSets = True; " +
+                          $"User ID = {Data.SystemVariables.SysUser}; " +
+                          $"Password ={Crypto.DecryptText(Data.SystemVariables.SysHash, Data.Global.Config + Data.Global.Catalog)}; " +
+                          $"MultipleActiveResultSets = True; App = EntityFramework; "
+                        : $"data source = MANUFACTURING; initial catalog = {Data.Global.Catalog}; " +
+                          $"integrated security = True; MultipleActiveResultSets = True; " +
+                          $"MultipleActiveResultSets = True; App = EntityFramework; "
+                }.ToString()
+            )
+        {
+        }
+    }
+}
