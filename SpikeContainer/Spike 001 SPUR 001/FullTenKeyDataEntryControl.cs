0c84d9ba4ec52678c183ab9fa2c2b88519b7c2e5
Jeff Hyder <JHyder@polartec.com>
2018-01-05 16:34:44 -0500

    e5 01-05-2018

--- SpikeContainer/Spike 001 SPUR 001/FullTenKeyDataEntryControl.cs
+++ SpikeContainer/Spike 001 SPUR 001/FullTenKeyDataEntryControl.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//******************************************************************************************************
+// TenKeypadContol1EventArgs.cs - PTM Custom Components: Assembly - PTM Custom Components: Solution - PTM Custom Components: Prioject
+//
+// Copyright © 2017, Polartec Tennesee Manufacturing LLC. All Rights Reserved.
+//
+// Unless agreed to in writing, the subject software distributed under the License is distributed on an
+// "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+// License for the specific language governing permissions and limitations.
+//
+//******************************************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
