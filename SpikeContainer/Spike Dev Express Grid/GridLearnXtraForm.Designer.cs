6aa7cd9ffadbecc58f5fb182e08e21097cd53fa2
Jeff Hyder <JHyder@polartec.com>
2018-04-05 20:00:43 -0400

    4-5-2018 FIRST

--- SpikeContainer/Spike Dev Express Grid/GridLearnXtraForm.Designer.cs
+++ SpikeContainer/Spike Dev Express Grid/GridLearnXtraForm.Designer.cs
@@ -28,151 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
-            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.gridControl2 = new DevExpress.XtraGrid.GridControl();
             this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.gridControl3 = new DevExpress.XtraGrid.GridControl();
-            this.gridView3 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.Index = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.GrossLbs = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.TareLbs = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.NetResultLbs = new DevExpress.XtraGrid.Columns.GridColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.GrossWeight = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.TareWeight = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.NetWeight = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gridControl2
+            // 
+            this.gridControl2.Location = new System.Drawing.Point(12, 12);
+            this.gridControl2.MainView = this.gridView2;
+            this.gridControl2.Name = "gridControl2";
+            this.gridControl2.Size = new System.Drawing.Size(1120, 441);
+            this.gridControl2.TabIndex = 1;
+            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView2});
+            this.gridControl2.FocusedViewChanged += new DevExpress.XtraGrid.ViewFocusEventHandler(this.gridControl2_FocusedViewChanged);
+            this.gridControl2.EditorKeyDown += new System.Windows.Forms.KeyEventHandler(this.gridControl2_EditorKeyDown);
+            this.gridControl2.EditorKeyUp += new System.Windows.Forms.KeyEventHandler(this.gridControl2_EditorKeyUp);
+            this.gridControl2.EditorKeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gridControl2_EditorKeyPress);
+            // 
+            // gridView2
+            // 
+            this.gridView2.ActiveFilterEnabled = false;
+            this.gridView2.GridControl = this.gridControl2;
+            this.gridView2.Name = "gridView2";
+            this.gridView2.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
+            this.gridView2.OptionsBehavior.KeepFocusedRowOnUpdate = false;
+            this.gridView2.OptionsEditForm.ShowOnEnterKey = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView2.OptionsMenu.EnableColumnMenu = false;
+            this.gridView2.OptionsMenu.ShowAutoFilterRowItem = false;
+            this.gridView2.OptionsMenu.ShowFooterItem = true;
+            this.gridView2.OptionsNavigation.AutoFocusNewRow = true;
+            this.gridView2.OptionsNavigation.EnterMoveNextColumn = true;
+            this.gridView2.OptionsView.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.VisibleAlways;
+            this.gridView2.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Bottom;
+            this.gridView2.OptionsView.ShowFooter = true;
+            this.gridView2.OptionsView.ShowGroupPanel = false;
+            this.gridView2.OptionsView.ShowPreviewRowLines = DevExpress.Utils.DefaultBoolean.False;
+            this.gridView2.OptionsView.ShowVerticalLines = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView2.OptionsView.ShowViewCaption = true;
+            this.gridView2.CustomDrawRowIndicator += new DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventHandler(this.gridView2_CustomDrawRowIndicator);
+            this.gridView2.ShownEditor += new System.EventHandler(this.gridView2_ShownEditor);
+            this.gridView2.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView2_FocusedRowChanged);
+            this.gridView2.FocusedColumnChanged += new DevExpress.XtraGrid.Views.Base.FocusedColumnChangedEventHandler(this.gridView2_FocusedColumnChanged);
+            this.gridView2.ValidateRow += new DevExpress.XtraGrid.Views.Base.ValidateRowEventHandler(this.gridView2_ValidateRow);
+            this.gridView2.RowUpdated += new DevExpress.XtraGrid.Views.Base.RowObjectEventHandler(this.gridView2_RowUpdated);
+            this.gridView2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gridView2_KeyDown);
+            this.gridView2.ValidatingEditor += new DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventHandler(this.gridView2_ValidatingEditor);
+            this.gridView2.RowCountChanged += new System.EventHandler(this.gridView2_RowCountChanged);
             // 
             // gridControl1
             // 
-            this.gridControl1.Location = new System.Drawing.Point(12, 536);
+            this.gridControl1.Location = new System.Drawing.Point(13, 522);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(1120, 275);
-            this.gridControl1.TabIndex = 0;
+            this.gridControl1.Size = new System.Drawing.Size(1120, 277);
+            this.gridControl1.TabIndex = 2;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
             // gridView1
             // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.GrossWeight,
+            this.TareWeight,
+            this.NetWeight});
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView1.OptionsBehavior.EditingMode = DevExpress.XtraGrid.Views.Grid.GridEditingMode.Inplace;
+            this.gridView1.OptionsBehavior.FocusLeaveOnTab = true;
+            this.gridView1.OptionsEditForm.ShowOnEnterKey = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView1.OptionsNavigation.AutoFocusNewRow = true;
+            this.gridView1.OptionsNavigation.AutoMoveRowFocus = false;
+            this.gridView1.OptionsNavigation.EnterMoveNextColumn = true;
+            this.gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Bottom;
+            this.gridView1.OptionsView.ShowFooter = true;
             // 
-            // gridControl2
+            // GrossWeight
             // 
-            this.gridControl2.Location = new System.Drawing.Point(13, 261);
-            this.gridControl2.MainView = this.gridView2;
-            this.gridControl2.Name = "gridControl2";
-            this.gridControl2.Size = new System.Drawing.Size(1120, 239);
-            this.gridControl2.TabIndex = 1;
-            this.gridControl2.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridView2});
+            this.GrossWeight.Caption = "Gross Weight";
+            this.GrossWeight.FieldName = "GrossWeight";
+            this.GrossWeight.Name = "GrossWeight";
+            this.GrossWeight.OptionsEditForm.StartNewRow = true;
+            this.GrossWeight.Visible = true;
+            this.GrossWeight.VisibleIndex = 0;
             // 
-            // gridView2
+            // TareWeight
             // 
-            this.gridView2.GridControl = this.gridControl2;
-            this.gridView2.Name = "gridView2";
-            this.gridView2.ValidatingEditor += new DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventHandler(this.gridView2_ValidatingEditor);
+            this.TareWeight.Caption = "Tare Weight";
+            this.TareWeight.FieldName = "TareWeight";
+            this.TareWeight.Name = "TareWeight";
+            this.TareWeight.Visible = true;
+            this.TareWeight.VisibleIndex = 1;
             // 
-            // gridControl3
+            // NetWeight
             // 
-            this.gridControl3.Location = new System.Drawing.Point(13, 13);
-            this.gridControl3.MainView = this.gridView3;
-            this.gridControl3.Name = "gridControl3";
-            this.gridControl3.Size = new System.Drawing.Size(1119, 200);
-            this.gridControl3.TabIndex = 2;
-            this.gridControl3.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
-            this.gridView3});
-            // 
-            // gridView3
-            // 
-            this.gridView3.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
-            this.Index,
-            this.GrossLbs,
-            this.TareLbs,
-            this.NetResultLbs});
-            this.gridView3.GridControl = this.gridControl3;
-            this.gridView3.Name = "gridView3";
-            this.gridView3.CustomDrawRowIndicator += new DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventHandler(this.gridView3_CustomDrawRowIndicator);
-            this.gridView3.ShownEditor += new System.EventHandler(this.gridView3_ShownEditor);
-            this.gridView3.ValidateRow += new DevExpress.XtraGrid.Views.Base.ValidateRowEventHandler(this.gridView3_ValidateRow);
-            this.gridView3.RowUpdated += new DevExpress.XtraGrid.Views.Base.RowObjectEventHandler(this.gridView3_RowUpdated);
-            this.gridView3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gridView3_KeyDown);
-            this.gridView3.ValidatingEditor += new DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventHandler(this.gridView3_ValidatingEditor);
-            // 
-            // Index
-            // 
-            this.Index.Caption = "Index";
-            this.Index.FieldName = "Index";
-            this.Index.Name = "Index";
-            this.Index.UnboundType = DevExpress.Data.UnboundColumnType.Integer;
-            this.Index.Visible = true;
-            this.Index.VisibleIndex = 0;
-            // 
-            // GrossLbs
-            // 
-            this.GrossLbs.Caption = "Gross Weight";
-            this.GrossLbs.FieldName = "GrossLbs";
-            this.GrossLbs.Name = "GrossLbs";
-            this.GrossLbs.UnboundType = DevExpress.Data.UnboundColumnType.Decimal;
-            this.GrossLbs.Visible = true;
-            this.GrossLbs.VisibleIndex = 1;
-            // 
-            // TareLbs
-            // 
-            this.TareLbs.Caption = "Tare Weight";
-            this.TareLbs.FieldName = "TareLbs";
-            this.TareLbs.Name = "TareLbs";
-            this.TareLbs.UnboundType = DevExpress.Data.UnboundColumnType.Decimal;
-            this.TareLbs.Visible = true;
-            this.TareLbs.VisibleIndex = 2;
-            // 
-            // NetResultLbs
-            // 
-            this.NetResultLbs.Caption = "Net Weight";
-            this.NetResultLbs.FieldName = "NetResultLbs";
-            this.NetResultLbs.Name = "NetResultLbs";
-            this.NetResultLbs.UnboundType = DevExpress.Data.UnboundColumnType.Decimal;
-            this.NetResultLbs.Visible = true;
-            this.NetResultLbs.VisibleIndex = 3;
+            this.NetWeight.Caption = "Net Weight Lbs";
+            this.NetWeight.FieldName = "NetWeight";
+            this.NetWeight.Name = "NetWeight";
+            this.NetWeight.Summary.AddRange(new DevExpress.XtraGrid.GridSummaryItem[] {
+            new DevExpress.XtraGrid.GridColumnSummaryItem(DevExpress.Data.SummaryItemType.Sum, "NetWeight", "SUM={0:0.##}")});
+            this.NetWeight.Visible = true;
+            this.NetWeight.VisibleIndex = 2;
             // 
             // GridLearnXtraForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1144, 846);
-            this.Controls.Add(this.gridControl3);
-            this.Controls.Add(this.gridControl2);
             this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.gridControl2);
             this.Name = "GridLearnXtraForm";
             this.Text = "GridLearnXtraForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GridLearnXtraForm_FormClosing);
             this.Load += new System.EventHandler(this.GridLearnXtraForm_Load);
             this.Shown += new System.EventHandler(this.GridLearnXtraForm_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridControl3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private DevExpress.XtraGrid.GridControl gridControl1;
-        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraGrid.GridControl gridControl2;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
-        private DevExpress.XtraGrid.GridControl gridControl3;
-        private DevExpress.XtraGrid.Views.Grid.GridView gridView3;
-        private DevExpress.XtraGrid.Columns.GridColumn Index;
-        private DevExpress.XtraGrid.Columns.GridColumn GrossLbs;
-        private DevExpress.XtraGrid.Columns.GridColumn TareLbs;
-        private DevExpress.XtraGrid.Columns.GridColumn NetResultLbs;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn GrossWeight;
+        private DevExpress.XtraGrid.Columns.GridColumn TareWeight;
+        private DevExpress.XtraGrid.Columns.GridColumn NetWeight;
     }
 }
--- SpikeContainer/Spike Dev Express Grid/GridLearnXtraForm.cs
+++ SpikeContainer/Spike Dev Express Grid/GridLearnXtraForm.cs
@@ -22,34 +22,34 @@ namespace SpikeContainer.Spike_Dev_Express_Grid
             InitializeComponent();
         }
 
-        List<Weights> _wEights = new List<Weights>();
-        List<WeightInGrid> _wIg = new List<WeightInGrid>();
-        List<GridWeightData> _gWd = new List<GridWeightData>();
+        List<WeightInGrid> _gWd = new List<WeightInGrid>();
+        //List<GridWeightData> _gWd = new List<GridWeightData>();
 
         private BaseEdit _deXEdit;//----------------    -   -   -  -The Indicator IF and Active Editor is selected in the DevExpress GridView
 
+
+        List<DevExpress.XtraGrid.Columns.GridColumn> _clms;
 
         #region [ FORM EVENT HANDLERS ]
 
         private void GridLearnXtraForm_Load(object sender, EventArgs e)
         {
             Trace.WriteLine($@"private void GridLearnXtraForm_Load(object sender, EventArgs e)");
+
+
+            InitializeData();
+            gridControl2.DataSource = _gWd;
         }
 
         private void GridLearnXtraForm_Shown(object sender, EventArgs e)
         {
             Trace.WriteLine($@"private void GridLearnXtraForm_Shown(object sender, EventArgs e)");
-
-            InitializeData();
-
-            gridControl3.DataSource = _wEights;
-            //gridControl3.DataMember = "Weights";
-
-            gridControl2.DataSource = _gWd;
-            //gridControl2.DataMember = "GridWeightData";
-
-            gridControl1.DataSource = _wIg;
-            //gridControl1.DataMember = "WeightInGrid";
+            gridView2.IndicatorWidth = 30;
+            
+            _clms = gridView2.Columns.ToList();
+            var col = _clms.Where(c => c.Name.Contains("NetResultLbs")).FirstOrDefault();
+            col.OptionsColumn.AllowEdit = false;
+            col.OptionsColumn.AllowFocus = false;
         }
 
 
@@ -63,53 +63,154 @@ namespace SpikeContainer.Spike_Dev_Express_Grid
 
         #endregion
 
-        #region [ GRID THREE (3) ]
-
-        private void gridView3_KeyDown(object sender, KeyEventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_KeyDown(object sender, KeyEventArgs e)");
-        }
-
-        private void gridView3_ShownEditor(object sender, EventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_ShownEditor(object sender, EventArgs e)");
-        }
-
-        private void gridView3_ValidatingEditor(object sender, DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_ValidatingEditor(object sender, DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventArgs e)");
-        }
-
-        private void gridView3_CustomDrawRowIndicator(object sender, DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_CustomDrawRowIndicator(object sender, DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventArgs e)");
-        }
-
-        private void gridView3_RowUpdated(object sender, DevExpress.XtraGrid.Views.Base.RowObjectEventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_RowUpdated(object sender, DevExpress.XtraGrid.Views.Base.RowObjectEventArgs e)");
-        }
-
-        private void gridView3_ValidateRow(object sender, DevExpress.XtraGrid.Views.Base.ValidateRowEventArgs e)
-        {
-            Trace.WriteLine($@"private void gridView3_ValidateRow(object sender, DevExpress.XtraGrid.Views.Base.ValidateRowEventArgs e)");
-        }
-
-        #endregion
-
 
 
 
         private void gridView2_ValidatingEditor(object sender, DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventArgs e)
         {
+            Trace.WriteLine($@"");
             Trace.WriteLine($@"private void gridView2_ValidatingEditor(object sender, DevExpress.XtraEditors.Controls.BaseContainerValidateEditorEventArgs e)");
+            Trace.WriteLine($@"{e.Value}");
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].GrossLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].TareLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].NetResultLbs}");
+
+
+            if (gridView2.FocusedColumn.Name.Contains("TareLbs"))
+            {
+
+            }
+        }
+
+        private void gridView2_RowUpdated(object sender, DevExpress.XtraGrid.Views.Base.RowObjectEventArgs e)
+        {
+            Trace.WriteLine($@"");
+            Trace.WriteLine($@"gridView2_RowUpdated");
+            Trace.WriteLine($@"{e.Row}");
+            Trace.WriteLine($@"{e.RowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].GrossLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].TareLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].NetResultLbs}");
+        }
+
+        private void gridView2_ValidateRow(object sender, DevExpress.XtraGrid.Views.Base.ValidateRowEventArgs e)
+        {
+            _gWd[e.RowHandle].NetResultLbs = _gWd[e.RowHandle].GrossLbs - _gWd[e.RowHandle].TareLbs;
+            Trace.WriteLine($@"");
+            Trace.WriteLine($@"gridView2_ValidateRow");
+            Trace.WriteLine($@"");
+
+            if (DialogResult.Yes == MessageBox.Show(this,"Click Yes to add a new Row, or Click No to quit.","Add New Row?", MessageBoxButtons.YesNo))
+            {
+                InitializeData();
+                gridView2.RefreshData();
+            }
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].GrossLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].TareLbs}");
+            Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].NetResultLbs}");
+        }
+
+
+
+
+        private void gridView2_RowCountChanged(object sender, EventArgs e)
+        {
+            Trace.WriteLine($@"gridView2_RowCountChanged");
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle]?.GrossLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle]?.TareLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle]?.NetResultLbs}");
+        }
+        private void gridView2_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            Trace.WriteLine($@"gridView2_FocusedRowChanged");
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].GrossLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].TareLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].NetResultLbs}");
+
+        }
+
+        private void gridView2_FocusedColumnChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedColumnChangedEventArgs e)
+        {
+            Trace.WriteLine($@"gridView2_FocusedColumnChanged");
+            var cs = gridView2.Columns.ToList();
+            Trace.WriteLine($@"{gridView2.FocusedColumn}");
+            Trace.WriteLine($@"{gridView2.FocusedRowHandle}");
+            Trace.WriteLine($@"{gridView2.FocusedValue}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].GrossLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].TareLbs}");
+            //Trace.WriteLine($@"{_gWd[gridView2.FocusedRowHandle].NetResultLbs}");
+        }
+
+
+
+
+        private void gridControl2_FocusedViewChanged(object sender, DevExpress.XtraGrid.ViewFocusEventArgs e)
+        {
+            Trace.WriteLine($@"gridControl2_FocusedViewChanged");
+        }
+
+
+
+        private void gridView2_ShownEditor(object sender, EventArgs e)
+        {
+            Trace.WriteLine($@"");
+            Trace.WriteLine($@"gridView2_ShownEditor");
+
+            if (gridView1.GetFocusedDataRow() == null)
+            {
+                
+            }
+        }
+
+
+
+
+        private void gridView2_KeyDown(object sender, KeyEventArgs e)
+        {
+            Trace.WriteLine($@"gridView2_KeyDown");
+        }
+
+        private void gridControl2_EditorKeyDown(object sender, KeyEventArgs e)
+        {
+            Trace.WriteLine($@"gridControl2_EditorKeyDown");
+        }
+
+        private void gridControl2_EditorKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Trace.WriteLine($@"gridControl2_EditorKeyPress");
+        }
+
+        private void gridControl2_EditorKeyUp(object sender, KeyEventArgs e)
+        {
+            Trace.WriteLine($@"gridControl2_EditorKeyUp");
         }
 
 
 
 
 
-
+        private void gridView2_CustomDrawRowIndicator(object sender, DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventArgs e)
+        {
+            Trace.WriteLine($@"gridView2_CustomDrawRowIndicator");
+            // set grid view row numbers in row header column
+            if (e.RowHandle >= 0)
+                e.Info.DisplayText = (e.RowHandle + 1).ToString();
+        }
 
 
 
@@ -124,14 +225,24 @@ namespace SpikeContainer.Spike_Dev_Express_Grid
 
         private void InitializeData()
         {
-            Trace.WriteLine($@"");
+            Trace.WriteLine($@"InitializeData  -- ADD NEW ROW");
 
-            _wEights.Add(new Weights(0, 0, 0));
+            //_gWd.Add(new GridWeightData(0, 0, 0));
+            _gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
+            //_gWd.Add(new WeightInGrid(0, 0, 0));
 
-            _wIg.Add(new WeightInGrid(0, 0, 0));
-
-            _gWd.Add(new GridWeightData(0, 0, 0));
         }
+
+
+
+
+
+
 
 
     }
